81f0d8942c071edd316aad09a58475599fc365e7
Cristobal Dupuis <cristobal.dupuis@gmail.com>
2026-05-20 22:52:22 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/growth-os/', // <--- This forces the browser to look in the right folder
 })

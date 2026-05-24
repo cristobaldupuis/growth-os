0d8ba52cfec67f8946cee12a23d442d282e42ab1
Cristobal Dupuis <cristobal.dupuis@gmail.com>
2026-05-24 01:00:27 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/growth-os/',
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'] // Explicitly tells the build tool to look for .jsx files
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   }
 })
